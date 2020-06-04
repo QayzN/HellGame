0240ce2ad08a68880db4b645cada65fcb6c39e00
Qayz Nurani <31449655+QayzN@users.noreply.github.com>
2020-06-04 01:03:44 -0700

    Game 0.0.1.1
    Trying to fix scene switching, was not succesful.
    Qayz pls fix

--- 2d menu/Assets/Scripts/MainMenu.cs
+++ 2d menu/Assets/Scripts/MainMenu.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
- 
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Game");
+        SceneManager.UnloadSceneAsync("Menu");
     }
 
     public void QuitGame()
